2a855e361e03f0c6c8d1729068b994eb3472a695
npoguca <npoguca@gmail.com>
2015-05-23 21:17:32 +0300

    Ok go

---
+++ battery/battery/battery.cpp
@@ -0,0 +1,279 @@
+// battery.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+
+using namespace std;
+
+SYSTEM_POWER_STATUS status;
+void Mario()
+{
+
+	while (true)
+	{
+		// note not LPSYSTEM_POWER_STATUS
+		Beep(659, 125);
+		Beep(659, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(523, 125);
+		Beep(659, 125);
+		Sleep(125);
+		Beep(784, 125);
+		Sleep(375);
+		Beep(392, 125);
+		Sleep(375);
+		Beep(523, 125);
+		Sleep(250);
+		Beep(392, 125);
+		Sleep(250);
+		Beep(330, 125);
+		Sleep(250);
+		Beep(440, 125);
+		Sleep(125);
+		Beep(494, 125);
+		Sleep(125);
+		Beep(466, 125);
+		Sleep(42);
+		Beep(440, 125);
+		Sleep(125);
+		Beep(392, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(125);
+		Beep(784, 125);
+		Sleep(125);
+		Beep(880, 125);
+		Sleep(125);
+		Beep(698, 125);
+		Beep(784, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(125);
+		Beep(523, 125);
+		Sleep(125);
+		Beep(587, 125);
+		Beep(494, 125);
+		GetSystemPowerStatus(&status);
+		if (status.ACLineStatus ==1)
+		{
+			return;
+		}
+		Sleep(125);
+		Beep(523, 125);
+		Sleep(250);
+		Beep(392, 125);
+		Sleep(250);
+		Beep(330, 125);
+		Sleep(250);
+		Beep(440, 125);
+		Sleep(125);
+		Beep(494, 125);
+		Sleep(125);
+		Beep(466, 125);
+		Sleep(42);
+		Beep(440, 125);
+		Sleep(125);
+		Beep(392, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(125);
+		Beep(784, 125);
+		Sleep(125);
+		Beep(880, 125);
+		Sleep(125);
+		Beep(698, 125);
+		Beep(784, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(125);
+		Beep(523, 125);
+		Sleep(125);
+		Beep(587, 125);
+		Beep(494, 125);
+		GetSystemPowerStatus(&status);
+		if (status.ACLineStatus ==1)
+		{
+			return;
+		}
+		Sleep(375);
+		Beep(784, 125);
+		Beep(740, 125);
+		Beep(698, 125);
+		Sleep(42);
+		Beep(622, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(415, 125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Sleep(125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Beep(587, 125);
+		Sleep(250);
+		Beep(784, 125);
+		Beep(740, 125);
+		Beep(698, 125);
+		Sleep(42);
+		Beep(622, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(698, 125);
+		Sleep(125);
+		Beep(698, 125);
+		Beep(698, 125);
+		GetSystemPowerStatus(&status);
+		if (status.ACLineStatus ==1)
+		{
+			return;
+		}
+		Sleep(625);
+		Beep(784, 125);
+		Beep(740, 125);
+		Beep(698, 125);
+		Sleep(42);
+		Beep(622, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(415, 125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Sleep(125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Beep(587, 125);
+		Sleep(250);
+		Beep(622, 125);
+		Sleep(250);
+		Beep(587, 125);
+		Sleep(250);
+		Beep(523, 125);
+		GetSystemPowerStatus(&status);
+		if (status.ACLineStatus ==1)
+		{
+			return;
+		}
+		Sleep(1125);
+		Beep(784, 125);
+		Beep(740, 125);
+		Beep(698, 125);
+		Sleep(42);
+		Beep(622, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(415, 125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Sleep(125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Beep(587, 125);
+		Sleep(250);
+		Beep(784, 125);
+		Beep(740, 125);
+		Beep(698, 125);
+		Sleep(42);
+		Beep(622, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(698, 125);
+		Sleep(125);
+		Beep(698, 125);
+		Beep(698, 125);
+		GetSystemPowerStatus(&status);
+		if (status.ACLineStatus ==1)
+		{
+			return;
+		}
+		Sleep(625);
+		Beep(784, 125);
+		Beep(740, 125);
+		Beep(698, 125);
+		Sleep(42);
+		Beep(622, 125);
+		Sleep(125);
+		Beep(659, 125);
+		Sleep(167);
+		Beep(415, 125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Sleep(125);
+		Beep(440, 125);
+		Beep(523, 125);
+		Beep(587, 125);
+		Sleep(250);
+		Beep(622, 125);
+		Sleep(250);
+		Beep(587, 125);
+		Sleep(250);
+		Beep(523, 125);
+		Sleep(625);
+	}
+}
+void March()
+{
+	while (true)
+	{
+		Beep(440, 500);
+		Beep(440, 500);
+		Beep(440, 500);
+		Beep(349, 350);
+		Beep(523, 150);
+		Beep(440, 500);
+		Beep(349, 350);
+		Beep(523, 150);
+		Beep(440, 1000);
+		Beep(659, 500);
+		Beep(659, 500);
+		Beep(659, 500);
+		Beep(698, 350);
+		Beep(523, 150);
+		Beep(415, 500);
+		Beep(349, 350);
+		Beep(523, 150);
+		Beep(440, 1000);
+		if (status.ACLineStatus ==0)
+		{
+
+		}
+	}
+	
+
+
+
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	FreeConsole();
+	while (true)
+	{
+		
+		GetSystemPowerStatus(&status);
+		if (status.ACLineStatus == 0)
+		{
+		
+			int f = rand() % 2 + 0;
+			if (f==0)
+			{
+				March();
+			}
+			else
+			Mario();
+		}
+		Sleep(10000);
+		
+	}
+
+	
+		return 0;
+}
+
